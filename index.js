6459e4bee23d1a4a612637165ec33c43f180b9c7
LAPTOP-SOURAV <1997mehra@gmail.com>
2020-06-17 22:23:24 +0530

    Updated index.js with correct build folder path

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ if(process.env.NODE_ENV === 'production')
 }
 
 app.get('*', (req, res) => {
-  res.sendfile(path.resolve(__dirname,'frontend','build','index.html'));
+  res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
 })
 
 app.listen(PORT,() =>{
